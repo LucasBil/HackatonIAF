6fb5ba14d828707bd1ab789d14184776398a06f3
DabaSD <dabasambdiagne30@gmail.com>
2025-02-13 13:29:42 +0000

    bot

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import ChatbotComponent from './components/ChatbotComponent.vue';
 </script>
 
 <template>
@@ -17,6 +18,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
+  <ChatbotComponent />
   <RouterView />
 </template>
 
